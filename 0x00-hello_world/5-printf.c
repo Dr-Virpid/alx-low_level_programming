af1a0061821aad37056bcff63c1d046d8e6a19cb
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-08 22:00:25 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,11 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - use printf to send the argument to the standard output 
+ * Return: 0 after successful completion
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0)
+}
