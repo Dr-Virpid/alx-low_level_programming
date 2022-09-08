6b2d61a40b85dcea8cdfd3c1cb3dd307c610da8c
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-08 22:35:21 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,1 +1,18 @@
-#!/bin/bash
+#include <stdio.h>
+
+/**
+ * main - prints the size of multiple data type on each computer
+ * Return: returns 0 after successful completion of all printf statements
+ */
+int main(void)
+{
+char c;
+int i;
+
+printf("Size of a char: %c byte(s)",);
+printf("Size of an int: %d byte(s)",);
+printf("Size of a long int: 4 byte(s)");
+printf("Size of a long long int: 8 byte(s)");
+printf("Size of a float: 4 byte(s)");
+return (0);
+}
