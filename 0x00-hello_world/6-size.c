99233ef1f014df9b1632b2503cf7de34696d387a
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-08 22:47:06 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ float d;
 long int a;
 long long int b;
 
-printf("Size of a char: %d byte(s)", sizeof(c));
-printf("Size of an int: %d byte(s)", sizeof(i));
-printf("Size of a long int: %d byte(s)", sizeof(a));
-printf("Size of a long long int: %d byte(s)", sizeof(b));
-printf("Size of a float: %d byte(s)", sizeof(d));
+printf("Size of a char: %d byte(s)\n", sizeof(c));
+printf("Size of an int: %d byte(s)\n", sizeof(i));
+printf("Size of a long int: %d byte(s)\n", sizeof(a));
+printf("Size of a long long int: %d byte(s)\n", sizeof(b));
+printf("Size of a float: %d byte(s)\n", sizeof(d));
 return (0);
 }
