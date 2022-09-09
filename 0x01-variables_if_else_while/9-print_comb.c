29fee263db58deee447c022d47d98462d178d1fe
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-09 17:22:01 +0100

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,1 +1,20 @@
 #include <stdio.h>
+
+/**
+ * main - prints out all single base 10 numbers using
+ * putchar and without declaring a type char
+ * Return: 0 after successful completion
+ */
+int main(void)
+{
+int a;
+
+for (a = 48; a <= 56; a++)
+{
+putchar(a);
+putchar(',');
+putchar(' ');
+}
+putchar('9');
+return (0);
+}
