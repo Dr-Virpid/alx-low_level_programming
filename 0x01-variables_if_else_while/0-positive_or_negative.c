e89a8d29cc20be795fe0768ade6caa7c022c9f73
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-09 15:07:04 +0100

    Update 0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,15 +2,27 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- *
- *
+ * main - checks if the random number generated is greater,less
+ * or equal to zero
+ * Return: 0 after successful completion of the if blocks
  */
 int main(void)
 {
 int n;
 
-\tsrand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
-	return (0);
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+if (n > 0)
+{
+printf("%d is positive\n", n);
+}
+else if (n == 0)
+{
+printf("%d is zero\n", n);
+}
+else if (n < 0)
+{
+printf("%d is negative\n", n);
+}
+return (0);
 }
