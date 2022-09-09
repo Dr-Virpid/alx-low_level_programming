3c549916fe0f9c2a937aad501512de84ab1063d8
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-09 23:06:13 +0100

    Update 100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,1 +1,34 @@
 #include <stdio.h>
+
+/**
+ * main - prints 2 unique digits from between 00 and 99
+ * i.e 01 is the same as 10 so don't put 10
+ * Return: 0 always
+ */
+int main(void)
+{
+int first = 48;
+int second = 48;
+
+while (first < 58)
+{
+while (second < 58)
+{
+if (first != second && first < second)
+{
+putchar(first);
+putchar(second);
+if (first == 57 && second == 56)
+{
+break;
+}
+putchar(',');
+putchar(' ');
+}
+
+second++;
+}
+first++
+}
+return (0);
+}
