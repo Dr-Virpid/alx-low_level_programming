60e89d69fc28b5e4670020af11b4e71ac253b681
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-09 17:14:45 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,1 +1,23 @@
 #include <stdio.h>
+
+/**
+ * main - prints out all single base 16 numbers in lowercase using
+ * putchar and without declaring a type char
+ * Return: 0 after successful completion
+ */
+int main(void)
+{
+int a;
+
+for (a = 48; a <= 57; a++)
+{
+putchar(a);
+}
+for (a = 61; a <= 66; a++)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
+
