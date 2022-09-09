da472cfac1ab8b8f88e57481eb49650b17128889
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-09 17:11:36 +0100

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,1 +1,19 @@
 #include <stdio.h>
+
+/**
+ * main - prints out all single base 10 numbers using
+ * putchar and without declaring a type char
+ * Return: 0 after successful completion
+ */
+int main(void)
+{
+int a;
+
+for (a = 122; a >= 97; a--)
+{
+putchar(a);
+}
+putchar('\n');
+return (0);
+}
+
