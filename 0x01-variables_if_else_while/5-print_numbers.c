06324115893b8334542ace06936216e457a4b35b
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-09 16:26:26 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,1 +1,15 @@
 #include <stdio.h>
+
+/**
+ * main - prints all single digit base 10 numbers
+ * Return: 0 after successful completion
+ */
+int main(void)
+{
+for (num = 0; num < 10; num++)
+{
+puts(num);
+}
+puts('\n');
+return (0);
+}
