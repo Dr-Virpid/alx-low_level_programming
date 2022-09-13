531a71d625dfd54d0231eb5dc14fea1ddba338ac
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-14 00:53:58 +0100

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,10 +8,22 @@
  */
 void print_to_98(int n)
 {
-while (n < 99)
+if (n < 98)
+{
+while (n < 98)
 {
 printf("%d, ", n);
 n++;
 }
+}
+else
+{
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+}
+}
+printf("%d", 98);
 _putchar('\n');
 }
