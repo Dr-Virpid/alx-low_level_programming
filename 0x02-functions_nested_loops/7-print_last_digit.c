31f0be519691b5ef28d868db3e07361fd62100f5
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-13 23:03:17 +0100

    Create 7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_last_digit - returns the last digit of an integer
+ * @c: integer input
+ * Return: the last digit of the input
+ */
+int print_last_digit(int c)
+{
+c %= 10;
+return (c);
+}
