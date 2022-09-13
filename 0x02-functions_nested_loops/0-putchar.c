aff2e32c3e77fd42afe10b83330f807040973ae5
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-13 17:45:21 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,17 @@
 #include <stdio.h>
-
+#include <main.h>
 /**
  * main - prints out a string
  * Return: returns 0 after completion
  */
 int main(void)
 {
+int i = 0;
+char letter[] = {'_','p','u','t','c','h','a','r'};
+while (i < 8)
+{
+d = letter[i];
+_putchar(d);
+}
 return (0);
 }
