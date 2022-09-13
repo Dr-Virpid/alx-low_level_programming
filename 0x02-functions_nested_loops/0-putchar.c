3cf37989110b7e21564ae7c07885d7d9d95ebc58
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-13 18:20:43 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 int i = 0;
-char letter[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\0'};
+char letter[8] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r'};
 while (i < 8)
 {
 char d = letter[i];
