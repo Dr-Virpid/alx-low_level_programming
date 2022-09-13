38e211728caabef7e54113f4a9a1fc302edc5533
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-14 00:34:42 +0100

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers together
+ * @num1: first number
+ * @num2: second number
+ * Return: result of the addition
+ */
+int add(int num1, int num 2)
+{
+int result = num1 * num2;
+return (result);
+/*
+if (result < -9)
+{
+_putchar(45);
+_putchar(-result / 10 + 48);
+_putchar(-result % 10 + 48);
+}
+else if (result > 99)
+{
+_putchar(result / 10 + 48);
+_putchar(result % 10 + 48);
+}
+else if (result > 9)
+{
+_putchar(result / 10 + 48);
+_putchar(result % 10 + 48);
+}
+ */
+}
