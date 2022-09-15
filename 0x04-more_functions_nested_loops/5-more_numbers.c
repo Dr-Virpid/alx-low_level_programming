4b14b9f4ec0b744fcadca882a6656c90c3c0fb80
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-15 20:54:14 +0100

    Create 5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14 10 times
+ * Return: none
+ */
+void more_numbers(void)
+{
+int i, j, k;
+for (i = 0; i < 9; i++)
+{
+for (j = 48; j < 59; i++)
+_putchar(j);
+}
+_putchar('\n');
+}
