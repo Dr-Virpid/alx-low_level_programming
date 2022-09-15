9ec8a81ed39ecc2dc92c13ae60ecaa6542a87ac1
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-15 16:44:17 +0100

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,6 +4,7 @@
  * _isupper - checks if input is an uppercase letter
  * @c: decimal equivalent of the character
  * Return: 1 if c is uppercase and 0 otherwise
+ */
 int _isupper(int c)
 {
 if (c < 64 && c > 91)
