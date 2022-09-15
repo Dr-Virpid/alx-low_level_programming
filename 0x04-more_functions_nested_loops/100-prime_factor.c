efd9c665262a67fd8e40748256903d02af094d03
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-16 00:48:42 +0100

    Create 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int prime(int a);
+
+/**
+ * main - finds the highest prime factor of a number
+ * Return: none
+ */
+void main(void)
+{
+long int i, j;
+int k;
+i = 612852475143;
+for (k = 3; k <= (i / 2); k++;)
+{
+if (i % k == 0 && prime(k) == 0)
+j = k
+}
+printf("%d", j);
+}
+
+/**
+ * prime - checks if a number is a prime number
+ * @a: number to be checked
+ * Return 0 if its a prime number and 1 otherwise
+ */
+int prime(int a)
+{
+int i;
+for (i = 2; i < a; i++)
+{
+if (a % i == 0)
+return (1);
+}
+return (0);
+}
