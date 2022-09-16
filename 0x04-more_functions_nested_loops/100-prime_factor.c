2f25d00da5001231a62c6f1d8edcaae3c17b6d3c
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-16 01:10:38 +0100

    Update 100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -4,34 +4,21 @@ int prime(int a);
 
 /**
  * main - finds the highest prime factor of a number
- * Return: none
+ * Return: 0 always
  */
 int main(void)
 {
-long int i, j;
-int k;
-i = 612852475143;
-for (k = 2; k <= (i / 2); k++)
-{
-if (i % k == 0 && prime(k) == 0)
-j = k;
-}
-printf("%li\n", j);
-return (0);
-}
+long int number, pfactor;
 
-/**
- * prime - checks if a number is a prime number
- * @a: number to be checked
- * Return 0 if its a prime number and 1 otherwise
- */
-int prime(int a)
+number = 612852475143;
+for (pfactor = 2; pfactor <= number; pfactor++)
 {
-int i;
-for (i = 2; i < a; i++)
+if (number % pfactor == 0)
 {
-if (a % i == 0)
-return (1);
+number /= pfactor;
+pfactor--;
 }
+}
+printf("%li\n", pfactor);
 return (0);
 }
