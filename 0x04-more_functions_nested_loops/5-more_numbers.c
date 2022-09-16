63fc8491fc04dee578140ac8fe64865535ef2421
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-16 01:32:05 +0100

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,11 +6,16 @@
  */
 void more_numbers(void)
 {
-int i, j, k;
-for (i = 0; i < 9; i++)
+int i, j;
+
+for (i = 0; i < 10; i++)
 {
-for (j = 48; j < 59; i++)
-_putchar(j);
+for (j = 0; j < 15; j++)
+{
+if (j >= 10)
+_putchar((j / 10) + 48);
+_putchar((j % 10) + 48);
 }
 _putchar('\n');
+}
 }
