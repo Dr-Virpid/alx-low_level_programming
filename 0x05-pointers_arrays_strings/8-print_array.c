ea1aed941be5539f95ca44d6b12f831b55b3c168
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-20 00:34:20 +0100

    Create 8-print_array.c

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_array - prints out all elements of the array
+ * @a: array of integers
+ * @n: length of the array
+ * Return: none
+ */
+void print_array(int *a, int n)
+{
+int i;
+_putchar(a[0]);
+for (i = 1; i < n; i++)
+printf(", %d", a[i]);
+}
