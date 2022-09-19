df00b1e5d07dc06dc84febc25c1e1449044f53d3
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-19 20:40:06 +0100

    Update 0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,2 +1,11 @@
 #include "main.h"
-#include <stdio.h>
+
+/**
+ * reset_to_98 - updates the value of an address using its pointer variable
+ * @n: a pointer variable
+ * Return: none
+ */
+void reset_to_98(int *n)
+{
+*n = 98;
+}
