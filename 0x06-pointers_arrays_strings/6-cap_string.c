f86cacffe1826e46c88be7c9bef74fdea5f6006c
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-22 17:14:10 +0100

    Update 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,6 +11,11 @@ char *cap_string(char *str)
 {
 int i = 0;
 
+if (str[i] > 96 && str[i] < 123)
+str[i] = str[i] - 32;
+
+i++;
+
 while (str[i] != '\0')
 {
 if (str[i] > 96 && str[i] < 123 && symb(*(str + i - 1)))
