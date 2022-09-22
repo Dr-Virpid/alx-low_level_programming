1a6972b774d0d392c29ff2aa226b3119010c07f8
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-22 22:25:15 +0100

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,16 +11,17 @@ char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
 
-while (i < n)
+while (src[i] != '\0')
 {
-/* if (*(dest + i) == '\0') */
-
-if (*(src + i) == '\0') 
-break;
-
 *(dest + i) = *(src + i);
-
 i++;
 }
+
+while (i < n)
+{
+src[i] = '\0';
+i++;
+}
+
 return (dest);
 }
