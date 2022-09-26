2c972f40385f834c7dcb530beda4da9e11b0ee09
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-26 16:52:58 +0100

    Create 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - searches for a character in a string
+ * @s: string to search
+ * @c: character to search for
+ * Return: address that points to the first occurrence of
+ * the character
+ */
+char *_strchr(char *s, char c)
+{
+int i = 0;
+
+while (s[i] != '\0')
+{
+if (s[i] == c)
+break;
+}
+return(s + i);
+}
