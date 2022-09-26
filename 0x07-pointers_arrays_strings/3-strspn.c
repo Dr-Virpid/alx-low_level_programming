e5c462ae372802517da0c025303154ee689dbadb
jesutofunmi <ogunbonajesutofunmi@gmail.com>
2022-09-26 11:41:24 -0700

    hot fix

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strspn - returns the nu+++++++++++++++++++++
+ * @s: first string
+ * @accept: second string
+ * Return: number of bytes in s that are also in accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+int i, j;
+int k = 0;
+
+for (i = 0; s[i] != ' ' && s[i] != '\0'; i++)
+{
+	for (j = 0; accept[j] != '\0'; j++)
+	{
+		if (s[i] == accept[j])
+			k += 1;
+	}
+}
+
+return (k);
+}
