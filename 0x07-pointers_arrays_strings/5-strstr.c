0fb3a7387ef655d1dde7711b3f85a795c14ba1a6
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-09-26 21:12:46 +0100

    file 7-print_chessboard.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strstr - finds the first occurrence of a substring in another string
+ * @haystack: string to search inside
+ * @needle: substring to search for
+ * Return: address of the first occurence or NULL if not found
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints out each character of a 2 dimensional array
+ * @a: pointer to an array
+ * Return: none
+ */
+void print_chessboard(char (*a)[8])
+{
+int j, i:
+
+for (i = 0; a[i] != '\0'; i++)
+{
+	for (j = 0; j < 8; j++)
+	{
+		_putchar(a[i][j]);
+	}
+	_putchar('\n');
+}
+
