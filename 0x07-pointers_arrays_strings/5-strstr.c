9c0dec4d2f290dc81f0573854334da8d0ea31bfd
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-09-26 23:53:38 +0100

    wrote code for file 5-strstr.c

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -8,5 +8,19 @@
  */
 char *_strstr(char *haystack, char *needle)
 {
-	
+int i, j;
+
+for (i = 0; haystack[i] != '\0'; i++)
+{
+	if (haystack[i] == needle[0])
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i + j] != needle[j]
+					return ('\0');
+		}
+		return (haystack + i);
+	}
+}
+return ('\0');
 }
