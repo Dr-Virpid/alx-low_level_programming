2afeb06de5c5347c43bd2c4fb4bc6217ab545f2b
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-09-26 22:37:19 +0100

    file 8-print_diagsums.c written

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,11 +13,11 @@ int sum1 = 0;
 int sum2 = 0;
 
 for (i = 0; i < size; i++)
-	sum1 += a[i][i];
+	sum1 += *((a + i*size) + i);
 i = 0;
 for (j = size -1; j >= 0; j--)
 {
-	sum2 += a[i][j];
+	sum2 += *(a + i*size + j);
 	i++;
 }
 printf("%i, %i\n", sum1, sum2);
