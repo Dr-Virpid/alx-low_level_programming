bee78c44bd8f9371b419539bd4477f91031b173b
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-26 16:10:02 +0100

    Update 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -2,11 +2,18 @@
 
 
 /**
- * _memset -
- *
+ * _memset - fills the first "n" characters of s with "b"
+ * @s: pointer to the string
+ * @b: character to replace with
+ * @n: number of characters to replace
  * Return:
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-
+int i;
+for (i = 0; i < n; i++)
+{
+s[i] = b;
+}
+return (s);
 }
