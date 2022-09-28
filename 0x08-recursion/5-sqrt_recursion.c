ad4e913353656d87dcb5f6bddb53f5337ba50fd4
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-28 23:11:15 +0100

    Create 5-sqrt_recursion.c

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - checks the input to see if its has a natural square root
+ * @n: number input
+ * Return: square root of the number or -1 if it doesn't have a
+ * natural square root
+ */
+int _sqrt_recursion(int n)
+{
+
+}
