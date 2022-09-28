9bcff60b2600adca60dc6674df721caef4c9f5ed
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-29 00:23:31 +0100

    Update 6-is_prime_number.c

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,7 @@
 #include "main.h"
 
+int prime_number(int, int);
+
 /**
  * is_prime_number - checks if a number is a prime number
  * @n: number input
@@ -10,5 +12,21 @@ int is_prime_number(int n)
 if (n <= 1)
 return (1);
 else
+return (prime_number(n, n-1));
+}
 
+/**
+ * prime_number - recursive function to calculate prime_number
+ * @n: prime_number
+ * @y: divisor
+ * Return: either 1 or 0
+ */
+int prime_number(int n, int y)
+{
+if (y == 1)
+return (1);
+else if (n % y != 0)
+return (1 * prime_number(n, y - 1));
+else
+return (0);
 }
