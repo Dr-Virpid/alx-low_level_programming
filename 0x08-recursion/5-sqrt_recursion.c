0faef55ef179efbe2304ef09e78eb27be0673448
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-09-29 01:22:59 +0100

    Update 5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-int _sqrt(long long int, long long int);
+long int _sqrt(long int, long int);
 
 /**
  * _sqrt_recursion - checks the input to see if its has a natural square root
@@ -21,7 +21,7 @@ return (-1);
  * @y: counter that reduces by one with each function call
  * Return: a number
  */
-int _sqrt(long long int n, long long int y)
+long int _sqrt(long int n, long int y)
 {
 if (y * y == n)
 return (-y);
