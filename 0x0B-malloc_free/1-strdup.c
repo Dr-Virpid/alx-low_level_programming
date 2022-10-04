e199d0b02f9971c5c823f7154233b9bdad453c35
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-10-04 16:01:28 +0100

    wrote code for file 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _strdup - duplicates string passed to it, returns address of the duplicate
+ * @str: string passed to it
+ * Return: address of the string or NULL if space could not be allocated or
+ * value of the string is NULl
+ */
+char *_strdup(char *str);
+{
+	if (str == NULL)
+		return (NULL);
+	int i, j;
+	char *ptr;
+
+	for (i = 0; str[i] != '\0'; i++)
+		continue;
+
+	ptr = malloc(sizeof(char) * i + 1);
+	if (ptr == NULL)
+		return (NULL);
+
+	for (j = 0; j =< i; j++)
+		ptr[j] = str[j];
+
+	return (ptr);
+
+}
