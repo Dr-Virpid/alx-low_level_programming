dd350fe67b1a3018e30ee1ab86f67b8f062d64cb
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-04 19:59:16 +0100

    Update 2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,8 +10,30 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i, j;
-	char *ptr;
+int i, j, k, l;
+char *ptr;
 
-	for (i = 0; s1[i] != 
+if (s1 == NULL)
+s1 = "";
+
+if (s2 == NULL)
+s2 = "";
+
+for (k = 0; s1[k] != '\0'; k++);
+
+for (l = 0; s1[l] != '\0'; l++);
+
+ptr = malloc(sizeof(char) * (k + l + 1));
+if (ptr == NULL)
+return (NULL);
+
+for (i = 0; s1[i] != '\0'; i++)
+ptr[i] = s1[i];
+
+for (j = 0; s2[j] != '\0'; j++, i++)
+ptr[i] = s2[j];
+
+ptr[i] = '\0';
+
+return (ptr);
 }
