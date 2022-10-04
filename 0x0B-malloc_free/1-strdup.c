0f08d5cde7b0f971549c04561de4626181eb8467
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-10-04 16:11:57 +0100

    fixed compilation bug in 1-strdup.c

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,22 +10,30 @@
 char *_strdup(char *str)
 {
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 
 	int i = 0;
 	int j = 0;
 	char *ptr;
 
 	for (i = 0; str[i] != '\0'; i++)
+	{
 		continue;
+	}
 
 	ptr = malloc(sizeof(char) * i + 1);
+
 	if (ptr == NULL)
+	{
 		return (NULL);
+	}
 
 	for (j = 0; j <= i; j++)
+	{
 		ptr[j] = str[j];
+	}
 
 	return (ptr);
-
 }
