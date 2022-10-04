6f2e580c95dd8066b0b38c7ab56a004fcc2f2b58
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-10-04 15:42:27 +0100

    rearranged code layout to pass betty check

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * create_array - creates an array of character ini+tialized with a specific char
+ * create_array - creates an array of character initialized with
+ * a specific char
  * @size: size of the array
  * @c: character to initialize the array with
  * Return: address of array or NULL if size is zero
@@ -11,17 +12,17 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i = 0;
 	char *ptr;
-	
+
 	if (size == 0)
 		return (NULL);
 
 	ptr = malloc(sizeof(char) * size);
 	if (ptr == NULL)
 		return (NULL);
-	
+
 	for (i = 0; i < size; i++)
 		ptr[i] = c;
 
 	return (ptr);
-	
+
 }
