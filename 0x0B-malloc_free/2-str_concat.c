3ef38200c333e3e35a47d003c8d75886ddef90c5
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-10-04 16:35:19 +0100

    hot fix

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * str_concat - concatenates two string and return the address of the
+ * new string
+ * @s1: first string
+ * @s2: second string
+ * Return: address of the new string or NULL if it failed
+ */
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *ptr;
+
+	for (i = 0; s1[i] != 
+}
