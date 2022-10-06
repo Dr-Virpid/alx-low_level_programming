38aaf2613905105e6a2439a17f1f4a1fc07d6f4a
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-06 15:11:02 +0100

    Update 0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -1,1 +1,21 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocates memory of size int b and returns the address
+ * or return an exit code of 98 if space could not be allocated
+ * @b: size of memory to be allocated
+ * Return: address to the allocated memory
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr;
+
+ptr = malloc(b);
+
+if (ptr == NULL)
+exit(98);
+
+return (ptr);
+
+}
