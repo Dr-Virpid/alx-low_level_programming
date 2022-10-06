26cb518bcf798268eb19f44b2b1727f01a3af74e
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-06 15:42:21 +0100

    Update 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,1 +1,40 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenate the first n characters
+ * of string 2 to string one and return the address of the new string
+ * @s1: first string
+ * @s2: second string
+ * @n: number of characters in s2 to concatenate to s1
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+unsigned int i, j, k, l;
+char *ptr;
+
+if (s1 = NULL)
+s1 = "";
+
+if (s2 = NULL)
+s2 = "";
+
+for (i = 0; s1[i] != '\0'; i++)
+;
+
+for (j = 0; s2[j] != '\0' && j < n; i++)
+;
+
+ptr = malloc(j + i + 1);
+if (ptr == NULL)
+return (NULL);
+
+for (k = 0; k < i; k++)
+ptr[k] = s1[k];
+
+for (l = 0; l < j; l++, k++)
+ptr[k] = s2[l];
+
+ptr[k] = '\0';
+
+return (ptr);
+}
