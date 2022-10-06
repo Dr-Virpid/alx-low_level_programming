5577949d657a921bf3babc15b5610e3f8a421bb0
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-06 20:20:52 +0100

    Update 100-realloc.c

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,1 +1,15 @@
 #include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - expands the memory allocated previously by malloc and returns
+ * the address to the expanded memory
+ * @ptr: address of the allocated memory
+ * @old_size: size of memory allocated to the address
+ * @new_size: size of memory to expand to
+ * Return: address of the expanded memory
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+
+}
