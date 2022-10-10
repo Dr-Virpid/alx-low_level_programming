c5a9580bdf14bd88d228e97a9e0ddda549f5e3f8
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-10 23:39:46 +0100

    Update 4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -13,7 +13,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *newdog;
 
-newdog = malloc(sizeof(*newdog));
+newdog = malloc(sizeof(dog_t));
 if (newdog == NULL || name == NULL || owner == NULL)
 return (NULL);
 
