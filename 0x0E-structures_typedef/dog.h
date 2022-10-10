7754c3b105c47839cb13cfc3cd89dec0c2e26b3c
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-10 22:01:08 +0100

    Create dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_
+#define _DOG_
+
+/**
+ * struct dog - defines a struct that represents the features
+ * of a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+}
+
+#endif /* _DOG_ */
