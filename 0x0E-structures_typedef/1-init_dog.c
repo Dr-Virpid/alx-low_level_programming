d9bda5ca6ed6ffe79608c2a4d661bb1759c04e76
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-10 22:33:27 +0100

    Update 1-init_dog.c

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -4,9 +4,9 @@
  * init_dog - initializes a struct variable with passed arguments
  * @d: struct variable
  * @name: element to be initialized in struct var
- * @float: element to be initialized in struct var
  * @age: element to be initialized in struct var
  * @owner: element to be initialized in struct var
+ *
  * Return: none
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
