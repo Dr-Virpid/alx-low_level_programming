cbff8b221d2abb1d20885769083d112d87bf461f
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-12 22:29:59 +0100

    Create 3-get_op_func.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,14 @@
+#include "3-calc.h"
+
+
+/**
+ * get_op_func - checks if the symbol is an allowed
+ * symbol and return a pointer to the corresponding function
+ * @s: symbol to be checked
+ * Return: pointer to the function or null if the operator
+ * is not allowed
+ */
+int (*get_op_func(char *s))(int, int)
+{
+
+}
