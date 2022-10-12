17758c1a418fc5ae5ff36cf4abc265033c45724b
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-12 16:08:09 +0100

    Update function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,8 +1,10 @@
 #ifndef FUNCTION_POINTERS
 #define FUNCTION_POINTERS
 
-void print_name(char *name, void (*f)(char *));
+typedef int size_t;
 
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
 
 
