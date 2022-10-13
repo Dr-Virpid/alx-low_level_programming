47e62e4911e785fdf0f815920c5dc3a6a8a11601
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-13 16:52:30 +0100

    Create 3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,28 @@
+#include "variadic_functions.h"
+
+
+/**
+ * print_all - prints out any type of datatype
+ * @format: format of each datatype
+ */
+void print_all(const char * const format, ...)
+{
+int i, j;
+char *typ = "cifs";
+
+i = 0;
+while (format[i] != '\0')
+{
+
+j = 0;
+while (typ[j] != '\0')
+{
+
+
+j++;
+}
+
+i++;
+}
+
+}
