b825c9e1af7ef820240ed8033368d60bd39d84c4
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-13 15:19:29 +0100

    Update 1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -4,7 +4,7 @@
 
 /**
  * print_numbers - prints out all the numbers separated by the separator char
- * @seperator: separator character(s)
+ * @separator: separator character(s)
  * @n: number of integers to print out
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
@@ -12,14 +12,17 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list args;
 
+if (n <= 0)
+return;
+
 va_start(args, n);
 
 for (i = 0; i < n; i++)
 {
-if (separator == NULL)
-printf("%d", va_arg(args, int));
-else if (i == n - 1)
+if (i == n - 1)
 printf("%d\n", va_arg(args, int));
+else if (separator == NULL)
+printf("%d", va_arg(args, int));
 else
 printf("%d%s", va_arg(args, int), separator);
 }
