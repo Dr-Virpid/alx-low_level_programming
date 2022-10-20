53cd110fa2fcc092a8f2e88a5e8e705c92d455c7
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-20 17:11:54 +0100

    created new header file lists.h

---
+++ 0x12-singly_linked_lists/lists.h
@@ -0,0 +1,13 @@
+#ifndef _LUST_H_
+#define _LUST_H_
+
+#include <stdlib.h>
+
+int _putchar(char);
+size_t print_list(const list_t *h);
+size_t list_len(const list_t *h);
+list_t *add_node(list_t **head, const char *str);
+list_t *add_node_end(list_t **head, const char *str);
+void free_list(list_t *head);
+
+#endif /* _LUST_H_ */
