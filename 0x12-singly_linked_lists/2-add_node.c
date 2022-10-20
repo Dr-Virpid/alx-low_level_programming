79467546a198d1c5288f09ad1562a9ab6cc1a2e5
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-21 00:28:35 +0100

    Wrote code for file 2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+
+/**
+ * add_node - adds a new node to the linked list
+ * @head: double pointer to a linked list
+ * @str: string to set the nodes str to
+ * Return: linked list
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+list_t *ptr;
+
+ptr = malloc(sizeof(list_t));
+if (ptr == NULL)
+return (NULL);
+
+ptr->str = strdup(str);
+ptr->len = strlen(str);
+ptr->next = *head;
+
+head = ptr;
+
+return (*head);
+
+}
