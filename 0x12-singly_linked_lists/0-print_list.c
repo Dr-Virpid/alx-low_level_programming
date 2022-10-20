5e4ba8130b1ae2162db3fa00e28ddac9cde06211
Dr Virpid <ogunbonajesutofunmi@gmail.com>
2022-10-20 21:58:50 +0100

    hot fix for 0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,4 +1,6 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
  * print_list - returns the number of nodes present in a linked list
@@ -8,17 +10,17 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int count, i;
+	unsigned int count;
 	list_t *ptr;
 
 	if (h == NULL)
 		return (0);
 
-	for (ptr = h; ptr->next == NULL; ptr = ptr->next, count++)
+	for (ptr = &h; ptr->next == NULL; ptr = ptr->next, count++)
 		if (ptr->str == NULL)
 			printf("[0] (nil)");
 		else
-			printf("[%lu] %s\n", ptr->len, ptr->str);
+			printf("[%u] %s\n", ptr->len, ptr->str);
 
 	return (count);
 }
