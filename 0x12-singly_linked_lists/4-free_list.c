9bfba687bb1dd9b2018b7d819ef553ec848ad9ce
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-21 01:37:28 +0100

    Create 4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * free_list - frees all allocated memory of a linked list
+ * @head: pointer to first node in the list
+ */
+void free_list(list_t *head)
+{
+list_t *temp
+
+for (; head != NULL; head = temp)
+{
+temp = head->next;
+free(head->str);
+free(head);
+}
+
+}
