db5424aa9a73c7d2e98db779ef1aa754a7cbb71c
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-24 15:27:26 +0100

    Create 9-insert_nodeint.c

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+
+/**
+ * insert_nodeint_at_index - inserts a new node at a particular index
+ * @head: double pointer to head of the linked list
+ * @idx: position in linked list to initialize the new node
+ * @n: value to initialize the new node with
+ * Return: return the address of the new node or NULL
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+listint_t *ptr, *nnode;
+unsigned int i;
+
+nnode = malloc(sizeof(listint_t));
+if (nnode == NULL)
+return (NULL);
+nnode->n = n;
+
+ptr = *head;
+for (i = 0; ptr != NULL && i < idx - 1; i++, ptr = ptr->next)
+;
+
+if (ptr == NULL && i < idx)
+return (NULL);
+else
+{
+nnode->next = ptr->next;
+ptr->next = nnode;
+}
+
+return (nnode);
+
+}
