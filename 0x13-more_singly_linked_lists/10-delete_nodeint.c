8db8b50263111138e9e5404b26c803ddf21465b2
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-10-24 16:39:40 +0100

    Create 10-delete_nodeint.c

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,34 @@
+#include "lists.h"
+
+
+/**
+ * delete_nodeint_at_index - delete a node at a particular index
+ * @head: double pointer to head of the list
+ * @index: index at which the node is located
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+listint_t *ptr, *temp;
+unsigned int i;
+
+if (*head == NULL)
+return (-1);
+
+ptr = *head;
+
+for (i = 0; ptr != NULL && i < index - 1; i++)
+ptr = ptr->next;
+
+if (!(ptr) || (ptr->next == NULL && index != 0)
+return (-1);
+
+if (index == 0 && ptr->next == NULL)
+free(ptr);
+else
+{
+temp = ptr->next;
+ptr->next = temp->next;
+free(temp);
+}
+
+return (1);
+}
