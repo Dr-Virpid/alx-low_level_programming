6c1ed513b4b04c683c449f1ff35bfd65cfd7c140
Ogunbona Tofunmi <108702628+Dr-Virpid@users.noreply.github.com>
2022-11-01 09:47:42 +0100

    Create 2-append_text_to_file.c

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * append_text_to_file - appends text to end of a file
+ * @filename: file to append to
+ * @text_content: text to append to file
+ *
+ * Return: 1 if successful or -1 otherwise
+ */
+int append_text_to_file(const char *filename, char *text_content)
+{
+int fd;
+size_t len;
+
+if (!filename)
+return (-1);
+
+fd = open(filename, O_WRONLY | O_APPEND, 0);
+if (fd == -1)
+return (-1);
+
+if (!text_content)
+text_content = "";
+
+len = strlen(text_content);
+
+if (write(filename, text_content, len) == -1)
+close(fd), return (-1);
+
+close(fd);
+return (1);
+}
